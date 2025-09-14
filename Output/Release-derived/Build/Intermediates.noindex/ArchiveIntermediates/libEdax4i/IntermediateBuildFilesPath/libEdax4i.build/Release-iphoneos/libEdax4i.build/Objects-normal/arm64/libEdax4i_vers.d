28ca733ed637044a01c778685239a05ed387aa64
lavox <rabo@mac.com>
2025-09-15 08:48:23 +0900

    add dSYM settings

---
+++ Output/Release-derived/Build/Intermediates.noindex/ArchiveIntermediates/libEdax4i/IntermediateBuildFilesPath/libEdax4i.build/Release-iphoneos/libEdax4i.build/Objects-normal/arm64/libEdax4i_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/atsushi/Documents/xcode_workspace/libEdax4i/edax-reversi/Output/Release-derived/Build/Intermediates.noindex/ArchiveIntermediates/libEdax4i/BuildProductsPath/Release-iphoneos/libEdax4i.framework/Modules/module.modulemap \
+  /Users/atsushi/Documents/xcode_workspace/libEdax4i/edax-reversi/Output/Release-derived/Build/Intermediates.noindex/ArchiveIntermediates/libEdax4i/IntermediateBuildFilesPath/libEdax4i.build/Release-iphoneos/libEdax4i.build/DerivedSources/libEdax4i_vers.c
